c4e84c94be93ef6f657470ea2ad3a3c56ee71f5e
indrapradesa <indrapradesa77@gmail.com>
2022-02-18 23:39:52 +0700

    menambahkan vue component

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue, js, ts, jsx, tsx}"],
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    require ('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
   ],
 }
