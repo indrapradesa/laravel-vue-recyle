34ba5f38ac0de14754a327943afd339f43130125
indrapradesa <indrapradesa77@gmail.com>
2022-02-20 00:58:00 +0700

    menambahkan reusable PageComponent

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,27 +1,7 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
-    </div>
-  </header>
-  <main>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">here</div>
-      <!-- /End replace -->
-    </div>
-  </main>
+  <page-component title="Dashboard"> Content </page-component>
 </template>
 
-<script>
-import { LockClosedIcon } from "@heroicons/vue/solid";
-
-export default {
-  name: "Dashboard",
-  components: {
-    LockClosedIcon,
-  },
-};
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
 </script>
-
-<style></style>
